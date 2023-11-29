9d0b34a5a22b53300d991c8165752b7c9d19a8e5
Botirjon <thedavlativich@gmail.com>
2023-11-29 21:00:10 +0500

    Time abstraction .net8

---
+++ TimeAbstraction/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+
+var timeStamp = TimeProvider.System.GetTimestamp();
+
+//await Task.Delay(1000);
+
+var elapsedTime = TimeProvider.System.GetElapsedTime(timeStamp);
+
+Console.WriteLine(elapsedTime);
+
+
